2619f45525ca379a2132aa19f2dcbde4d22fde17
vijay22uk <vpandey@merrillcorp.com>
2017-04-03 12:56:09 +0530

    react + babel + webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+import * as ReactDom from 'react-dom'
+
+ReactDom.render(
+    <h1>Hello</h1>,
+    document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'src/public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|server|public)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
+
+module.exports = config;
